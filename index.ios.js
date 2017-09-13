2e5f594c37d2a878c9f70e26d3d56feffbed9cab
Senthil Kumaran <ursenix@gmail.com>
2017-09-13 22:37:15 +0530

    index-ios-updated

--- index.ios.js
+++ index.ios.js
@@ -11,43 +11,16 @@ import {
   Text,
   View
 } from 'react-native';
+import Component2 from './app/components/component2/component2';
 
 export default class firstreactapp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <Component2 />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('firstreactapp', () => firstreactapp);
